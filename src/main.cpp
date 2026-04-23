82f61b247bba9a09534642c5c832be42e20d9503
avr_dude <creed_truman77@my.albemarle.edu>
2026-04-23 11:45:29 -0400

    Testing 2

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 int main(void){
   initSerial9600();
-  serialTransmitMsg('A', 66);
+  serialTransmitMsg('A', 20);
+  serialTransmitMsg('Z', 90);
+  serialTransmitMsg('S', 200);
+  serialTransmitMsg('<', 36);
   while(true){}
 }
